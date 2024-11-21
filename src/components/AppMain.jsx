2b05d1c9c8f1bd78efa6db4d58287afff5f01081
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-11-21 18:17:06 +0100

    project done

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,25 +1,111 @@
+import { useState, useEffect} from 'react'
+
 export default function AppMain(){
-    return (
-        <main>
-            <h1>Todolist</h1>
-      <form>
-        <div className='mb-3'>
-          <label htmlFor="task" className='form-control'></label>
 
-          <div className='input-group mb-3'>
-            <input type="text" className='form-control' placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby='button-addon2'/>
-            <button className='btn btn-outline-secondary' type='button' id='button-addon2'>Button</button>
-          </div>
+  const initialTasks = ['Learn JS','Learn PHP','Learn Laravel','Learn Python','Learn C#','Learn C++','Learn AI'];
 
-          <small id='taskHelperId' className='form-text text-muted'>Type your new task</small>
+  const [tasks, setTasks] = useState(initialTasks);  //tasks is the array, setTasks to edit it, initialTasks initial data
+  const [newTask, setNewTask] = useState('');
+  const [searchText, setSearchText] = useState('');
+  const [filteredTasks, setFilteredTasks] = useState(tasks);
+
+  useEffect(()=>{
+    const filteredTasks = tasks.filter((item,index)=> item.toLowerCase().includes(searchText.toLowerCase()));
+    console.log(filteredTasks);
+    setFilteredTasks(filteredTasks);
+  }, [tasks, searchText]  //with [] useEffect is executed only once when the component is first mounted
+  );     //useEffect needs to run every time either tasks or searchText is updated
+
+
+  function addTask(e){
+    e.preventDefault();  //prevent the page refresh
+    if(newTask.trim() === '') {  //avoid the insertion of empty tasks.
+      return; 
+    }
+    const newTasks_local= [
+      newTask,
+      ...tasks,
+    ];
+    console.log(`added new Task ${newTask}`);
+    setTasks(newTasks_local);
+    setNewTask('');  //clean input
+  }
+
+  function handleTrashTaskClick(e){
+    const taskIndexToTrash = Number(e.target.getAttribute('data-index'));
+    //console.log(tasks, taskIndexToTrash);
+    const newTasks_local = tasks.filter((item,index)=> index !== taskIndexToTrash);
+    console.log(newTasks_local);
+    setTasks(newTasks_local);
+  }
+
+  function handleSearchForm(e){
+    e.preventDefault();
+    alert('Form sent');
+  }
+
+  function addImage(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
+  return(
+    <main>
+      <div className='container'>
+
+        {/* H1 + Search  */}
+        <div className='d-flex align-items-center justify-content-between'>
+          <h1>Todolist</h1>
+          <form onSubmit={handleSearchForm}>
+            <div className='mb-3'>
+              <input 
+                type="search"
+                className='form-control'
+                name='searchText'
+                id='searchText'
+                aria-describedby='searchHelper'
+                placeholder='🔍 Search ...' 
+                value={searchText}
+                onChange={e=> setSearchText(e.target.value)}
+              />
+            </div>
+          </form>
         </div>
-      </form>
 
-      <ul className='list-group list-group-numbered'>
-        <li className='list-group-item'>Active item</li>
-        <li className='list-group-item'>Item</li>
-        <li className='list-group-item'>Disabled item</li>
-      </ul>
-        </main>
-    );
+        {/* Add New Task  */}
+        <form onSubmit={addTask}>  {/* onSubmit event is tied to addTask funct */}
+          <div className='mb-3'>
+            <label htmlFor="task" className="form-label">Enter a new Task</label>
+              {/* Etichetta per il campo di input, htmlFor="task" link this label to input with id="task" */}
+            <div className='input-group mb-3'>
+              <input type="text" 
+                className='form-control'  //bootstrap style
+                placeholder="Recipient's username"
+                aria-label="Recipient's username"  //useful for screen readers
+                aria-describedby='button-addon2'  //for screen readers, link this input to elem with id='button-addon2' (the addTask button)
+                value={newTask}
+                onChange={e=>setNewTask(e.target.value)}  //dinamic edit setNewTask edita il value
+              />
+              <button className='btn btn-outline-secondary' type='submit' id='button-addon2'>Add Task</button>
+                {/* type='submit' send the form execute addTask */}
+            </div>
+
+            <small id='taskHelperId' className='form-text text-muted'>Type your new task</small>  {/* little instruction under form */}
+          </div>
+        </form>
+
+        {/* All Tasks */}
+        <ul className='list-group '>
+          {filteredTasks.map((item,index)=>
+            <li key={index} className='list-group-item d-flex justify-content-between'>
+              {item}
+              <button onClick={handleTrashTaskClick} data-index={index} className='btn btn-danger'>
+                <i className='bi bi-trash'></i>
+              </button>
+            </li>
+          )}
+        </ul>
+      </div>
+    </main> 
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
